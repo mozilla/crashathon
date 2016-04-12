bd8becd3542e31ee0503296ca94e1e37bd91034d
Allen Short <ashort@mozilla.com>
2016-04-11 22:16:13 -0500

    fix ALLOWED_HOSTS

--- crashathon/settings.py
+++ crashathon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='change me to a real secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
